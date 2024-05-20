e7897d7eb894b04ba2ed9931391ebd94f5a55537
Marcelo Corpucci <mcorpucci@gmail.com>
2024-05-20 17:03:44 -0300

    Added scenario for adding books to cart.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -17,7 +17,12 @@ describe('Home Page', () => {
           .should('have.attr', 'type')
           .and('equal', 'search')
       })
-      
+
+      it('can add a book to cart', () => {
+        cy.wait(2500)
+        cy.get('//div[@class="d-flex justify-content-start card-deck-container mb-4 ng-star-inserted"]/div[@class="p-1 ng-star-inserted"][1]/app-book-card[@_ngcontent-pnw-c64=""]/mat-card[@class="mat-mdc-card mdc-card book-card mat-elevation-z12"]/mat-card-content[@class="mat-mdc-card-content"]/app-addtocart[@_ngcontent-pnw-c31="" and @_nghost-pnw-c26=""]/button[@class="mdc-button mdc-button--raised mat-mdc-raised-button mat-primary mat-mdc-button-base"]/span[@class="mat-mdc-button-touch-target"]').click()              
+      })
+    
     })
   
 })
