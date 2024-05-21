cbdcfbe6527396e7408b28862c0e9496dfe633ff
Marcelo Corpucci <mcorpucci@gmail.com>
2024-05-21 15:12:46 -0300

    Major refactor. Added cart related scenarios.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,12 +1,10 @@
 describe('Home Page', () => {
 
   beforeEach('user is able to open the home page properly', () => {
-    cy.log('testing')
-    cy.log(Cypress.env('main_url'))
-    cy.visit('https://bookcart.azurewebsites.net/') //Cypress.env('main_url')
+    cy.visit('/')
   })
     context('user finds a toolbar containing different options', () => {
-      it('shows a toolbar', () => {
+      /*it('shows a toolbar', () => {
         cy.get('.mat-toolbar-row').should((toolbar) => {
           expect(toolbar).to.exist
         })  
@@ -16,13 +14,25 @@ describe('Home Page', () => {
         cy.get('input[placeholder="Search books or authors"]') 
           .should('have.attr', 'type')
           .and('equal', 'search')
-      })
+      })*/
+    })
 
+    context('User can add a Book to the Cart', () => {
       it('can add a book to cart', () => {
-        cy.wait(2500)
-        cy.get('//div[@class="d-flex justify-content-start card-deck-container mb-4 ng-star-inserted"]/div[@class="p-1 ng-star-inserted"][1]/app-book-card[@_ngcontent-pnw-c64=""]/mat-card[@class="mat-mdc-card mdc-card book-card mat-elevation-z12"]/mat-card-content[@class="mat-mdc-card-content"]/app-addtocart[@_ngcontent-pnw-c31="" and @_nghost-pnw-c26=""]/button[@class="mdc-button mdc-button--raised mat-mdc-raised-button mat-primary mat-mdc-button-base"]/span[@class="mat-mdc-button-touch-target"]').click()              
+        cy.getABook().then(bookNumber => {
+          cy.log(`Book Number: ${bookNumber}`)
+        
+          cy.getBookNameUsing(bookNumber).then(selectedBook => {
+            cy.log(`Selected book: ${selectedBook}`)
+            
+            cy.addBookToCartUsing(bookNumber)
+            cy.goToCart()
+            cy.verifyBookIsAddedToCart(selectedBook)
+          })
+
+        })
+
       })
-    
     })
   
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,40 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+function getABook() {
+    /*cy.get('.col')
+        .find('.d-flex')
+        .then(($value) => {
+            return $value.length
+        })*/
+    
+    return Math.floor(Math.random() * (44 - 1) + 1);
+}
+Cypress.Commands.add('getABook', getABook);
+
+Cypress.Commands.add('getBookNameUsing', bookNumber => {
+    cy.get(`:nth-child(${bookNumber}) > app-book-card > .mat-mdc-card > .mat-mdc-card-content > .card-title > a > strong`).then(bookTitle => {
+        return bookTitle.text()
+    })
+});
+
+Cypress.Commands.add('addBookToCartUsing', bookNumber => {
+    cy.get(`:nth-child(${bookNumber}) > app-book-card > .mat-mdc-card > .mat-mdc-card-content > app-addtocart > .mdc-button > .mdc-button__label`).should(book => {
+        expect(book).to.exist
+    }).then(button => {
+        cy.wrap(button).click()
+    })
+})
+
+Cypress.Commands.add('goToCart', () => {
+    cy.get(".mdc-icon-button > .mat-mdc-button-touch-target").then(cart => {
+        cart.click()
+    })
+})
+
+Cypress.Commands.add('verifyBookIsAddedToCart', (selectedBook) => {
+    cy.get('.cdk-column-title > a').invoke('text').should((cartItem) => {
+        expect(cartItem).to.equal(selectedBook)
+      })
+})
